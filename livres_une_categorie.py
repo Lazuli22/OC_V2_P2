c172514bceb595fe12b6c237f3526d6bea0681d9
Dolores DIAZ <diaz.dolores@gmail.com>
2021-01-06 03:53:39 +0100

    Scan l'ensemble des libres sur plusieurs pages

--- livres_une_categorie.py
+++ livres_une_categorie.py
@@ -2,13 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 from un_livre import infos_book, imprime_infos
+from math import *
 
 
 def livres_categorie(url):
+    nbre_livres = 0
     liste_livres = []
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
+        nbre_livres = soup.select_one("#default > div > div > div > div > form > strong:nth-child(2)").text
+        nbre_pages = ceil(int(nbre_livres)/20)
+        print(nbre_pages)
         liste_articles = soup.findAll("article")
         for i in range(1, len(liste_articles)+1):
             url_livre = "http://books.toscrape.com/catalogue/" + soup.select_one("#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child("+str(i)+") > article > h3 > a")['href'].strip("../../../")
@@ -17,8 +22,24 @@ def livres_categorie(url):
             #print("\n")
             #print(infos_book(url_livre))
             liste_livres.append(infos_book(url_livre))
+        if nbre_pages > 1:
+            for i in range(2, nbre_pages+1):
+                nurl = url.replace("index.html", "page-"+str(i)+".html")
+                res = requests.get(nurl)
+                soup2 = BeautifulSoup(res.text, "html.parser")
+                liste_articles = soup2.findAll("article")
+                for i in range(1, len(liste_articles)+1):
+                    url_livre = "http://books.toscrape.com/catalogue/" + soup2.select_one("#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child("+str(i)+") > article > h3 > a")['href'].strip("../../../")
+                    #print("-----------------")
+                    #print(url_livre)
+                    #print("\n")
+                    #print(infos_book(url_livre))
+                    liste_livres.append(infos_book(url_livre))
+            
     print(len(liste_livres))
     return liste_livres
 
 
-imprime_infos(livres_categorie("http://books.toscrape.com/catalogue/category/books/travel_2/index.html"), 'des_livres')
+#livres_categorie("http://books.toscrape.com/catalogue/category/books/travel_2/index.html")
+#livres_categorie("http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html")
+imprime_infos(livres_categorie("http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html"), 'des_livres')
